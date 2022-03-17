8e97bd48e9b0092ac7a7db04c64ef5b80ab4f094
Louis <louis.milhaud@gmail.com>
2022-03-17 16:48:14 +0100

    starting simplex noise 2D implementation

---
+++ tests/test_simplex.py
@@ -0,0 +1,33 @@
+"""contract.cairo test file."""
+import os
+
+import pytest
+from starkware.starknet.testing.starknet import Starknet
+
+# The path to the contract source code.
+CONTRACT_FILE = os.path.join("contracts", "simplex_noise.cairo")
+
+@pytest.mark.asyncio
+async def test_grad3():
+    """Test grad3 method."""
+    starknet = await Starknet.empty()
+    contract = await starknet.deploy(
+        source=CONTRACT_FILE,
+    )
+
+    l1 = await contract.grad3(0, 0).call()
+    l2 = await contract.grad3(0, 2).call()
+
+    assert l1.result == (1,)
+    assert l2.result == (0,)
+
+@pytest.mark.asyncio
+async def test_test_fixed():
+    starknet = await Starknet.empty()
+    contract = await starknet.deploy(
+        source=CONTRACT_FILE,
+    )
+
+    l1 = await contract.test_fixed(0).call()
+    print(l1.result)
+
